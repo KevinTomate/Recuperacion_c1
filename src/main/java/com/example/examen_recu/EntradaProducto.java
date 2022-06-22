566b3fd6f919225d4669313479898cb6b73a34e4
KevinTomate <101586778+KevinTomate@users.noreply.github.com>
2022-06-21 18:38:24 -0600

    Add files via upload

---
+++ src/main/java/com/example/examen_recu/EntradaProducto.java
@@ -0,0 +1,28 @@
+package com.example.examen_recu;
+
+public class EntradaProducto {
+
+    private float pventa;
+    private float pcompra;
+    private float cantidad;
+
+    public EntradaProducto(float pventa, float pcompra, float cantidad) {
+        this.pventa = pventa;
+        this.pcompra = pcompra;
+        this.cantidad = cantidad;
+    }
+
+    public float calcularPrecioVenta(){
+        float rVenta = pventa * cantidad;
+        return rVenta;
+    }
+    public float calcularPrecioCompra(){
+        float rCompra = pcompra * cantidad;
+        return rCompra;
+    }
+    public float calcularGanacia(float venta, float compra){
+        float rGanancia = venta - compra;
+        return rGanancia;
+    }
+
+}
